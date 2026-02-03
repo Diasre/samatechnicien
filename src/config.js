63ef1dd5a3eb9678dd9623002163683470275906
seck <j0h@example.com>
2026-02-02 20:24:11 -0800

    Mise à jour automatique

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-const API_URL = 'http://10.28.236.131:8080'; // Adresse IP de la machine pour le test mobile local
+// Utiliser l'URL définie dans les variables d'environnement (pour la production)
+// OU utiliser l'adresse locale par défaut (pour le développement)
+const API_URL = import.meta.env.VITE_API_URL || 'http://10.28.236.131:8080';
 
 export default API_URL;
